b7f2dbd5dff9ecb492f754cf6445fcd4c465490d
Selmane Zerradi <szerradi@hps-worldwide.com>
2023-06-27 11:29:36 +0100

    comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,10 @@
+/**
+Comment */
 package ma.enset;
 public class Test{
     public static void main(String[] args){
+        /**
+        main  */
         System.out.println("Hello World!");
     }
 }
